93a5a3b04ce71439519d0475efe54c2fcc8c4e90
Gabriel Macena <135872656+gbmacena@users.noreply.github.com>
2024-07-16 12:35:28 -0300

    Mudando placeholder

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
       <h1 className="primary-heading">Tem alguma pergunta?</h1>
       <h1 className="primary-heading">Deixe-nos ajudá-lo</h1>
       <div className="contact-form-container">
-        <input type="text" placeholder="yourmail@gmail.com" />
+        <input type="text" placeholder="seuemail@gmail.com" />
         <button className="secondary-button">Enviar</button>
       </div>
     </div>
